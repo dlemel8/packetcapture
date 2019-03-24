5ebfc9f3b73da76f1a6f8a4fbec83ae0f5ea7e5f
dlemel8 <dlemel8@gmail.com>
2019-03-24 08:05:54 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-if [[ -z ${PFRING_DIR} ]]; then
-        echo "please set PFRING_DIR to pfring source dir"
-        exit 1
-fi
-echo "PFRING_DIR: ${PFRING_DIR}"
-CGO_CFLAGS="-I${PFRING_DIR}/userland/lib -I${PFRING_DIR}/kernel" CGO_LDFLAGS="-L${PFRING_DIR}/userland/lib" go build packetcapture
-echo "DONE"
